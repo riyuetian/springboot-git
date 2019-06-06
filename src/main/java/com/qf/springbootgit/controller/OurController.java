707a004860defd2240b3f6a57706897730202418
riyuetian <916399746@qq.com>
2019-06-06 15:16:46 +0800

    update

--- src/main/java/com/qf/springbootgit/controller/OurController.java
+++ src/main/java/com/qf/springbootgit/controller/OurController.java
@@ -3,10 +3,17 @@ package com.qf.springbootgit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("our")
 public class OurController {
 
+   @RequestMapping("hello")
+   @ResponseBody
+    public String hello() {
+        System.out.println("springboot-big hello!!");
+        return "hello";
 
+    }
 }
